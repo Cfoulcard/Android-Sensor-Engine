20a6e9bb4f328910a3c9ffd49f5be40293c5ea66
Taterchip <cfoulcard@gmail.com>
2019-04-16 19:24:24 -0500

    Testing Intents
    To Be Continued

--- app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.soundtechsensors;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnContextClickListener;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensor_selection);
     }
+
+    /**
+     * When tapping the sound icon, this will open the Sound Sensor Activity
+     */
+
+    /**
+    ImageView soundIcon = (ImageView) findViewById(R.id.soundIcon);
+    soundIcon.OnContextClickListener(new View.OnContextClickListener() {
+
+        public void onClick(View view) {
+        Intent soundIntent = new Intent(this, SoundActivity.class);
+        startActivity(soundIntent);
+    }
+    }
+     */
 }
