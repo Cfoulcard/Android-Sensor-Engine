c1f0b9d14d704f99d74e90c8cc4451cf316c4275
Taterchip <cfoulcard@gmail.com>
2019-04-22 19:54:52 -0500

    Created SoundActivity
    Currently a blank slate, will add features in the future

--- app/src/main/java/com/example/android/soundtechsensors/SoundActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/SoundActivity.java
@@ -1,4 +1,13 @@
 package com.example.android.soundtechsensors;
 
-public class SoundActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sound_sensor);
+    }
 }
