0b6f24cb2b42f7d0cffef4d09df98b9bb3749300
Taterchip <cfoulcard@gmail.com>
2019-04-29 23:14:38 -0500

    Added action menu button

--- app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.android.soundtechsensors;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensor_selection);
+    }
+
+    //This will add functionality to the menu button within the action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
     }
 
     /**
