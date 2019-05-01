0228211634d697c426e1b6671ca0bb1b3956ee0f
Taterchip <cfoulcard@gmail.com>
2019-04-30 19:57:27 -0500

    Added Mediaplayer
    Mediaplayer to add main functionality to the app. Will have to play around with it a bit

--- app/src/main/java/com/example/android/soundtechsensors/SoundActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/SoundActivity.java
@@ -1,15 +1,21 @@
 package com.example.android.soundtechsensors;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
 
 public class SoundActivity extends AppCompatActivity {
+
+    //Initiates the Media Player to play raw files
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sound_sensor);
+        mp = MediaPlayer.create(this, R.raw.lightningsoundtest);
     }
 
     //This will add functionality to the menu button within the action bar
@@ -18,6 +24,11 @@ public class SoundActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.navigation_menu, menu);
         return true;
+    }
+
+    //Sound Test
+    public void playSound(View v) {
+        mp.start();
     }
 
 }
