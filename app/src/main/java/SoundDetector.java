b389bf9b44c3fcf13309d851f76f48db94e3521b
Taterchip <cfoulcard@gmail.com>
2019-05-14 20:22:16 -0500

    Create SoundDetector.java
    Will be used to detect sound

---
+++ app/src/main/java/SoundDetector.java
@@ -0,0 +1,40 @@
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+public class SoundDetector {
+
+    private MediaRecorder mRecorder = null;
+
+    public void start() {
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mRecorder.start();
+        }
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  mRecorder.getMaxAmplitude();
+        else
+            return 0;
+
+    }
+}
