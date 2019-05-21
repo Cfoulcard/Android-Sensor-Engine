5351ab039e2a9b243746d8a2e270a79683839338
Taterchip <cfoulcard@gmail.com>
2019-05-20 21:27:42 -0500

    Moved File
    Moved file for use with other activities

---
+++ app/src/main/java/com/example/android/soundtechsensors/SoundDetector.java
@@ -0,0 +1,44 @@
+package com.example.android.soundtechsensors;
+
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+//Audio cannot run background as of Android 9 (API level 28)
+
+public class SoundDetector {
+
+    private MediaRecorder mRecorder = null;
+
+    public void start() {
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mRecorder.start();
+        }
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  mRecorder.getMaxAmplitude();
+        else
+            return 0;
+
+    }
+}
