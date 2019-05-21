74ea6f011789e3a035bafafc5acef932f636ce17
Taterchip <cfoulcard@gmail.com>
2019-05-20 21:27:13 -0500

    Integrated SoundDetector.Java

--- app/src/main/java/com/example/android/soundtechsensors/SoundActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/SoundActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.soundtechsensors;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
@@ -10,6 +14,35 @@ public class SoundActivity extends AppCompatActivity {
 
     //Initiates the Media Player to play raw files
     MediaPlayer mp;
+    SoundDetector soundDetector;
+
+
+/* Here, this is the current activity
+if (ContextCompat.checkSelfPermission(this,
+    Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED) {
+
+        // Permission is not granted
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.RECORD_AUDIO)) {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+        } else {
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+    } else {
+        // Permission has already been granted
+    }
+**/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +59,17 @@ public class SoundActivity extends AppCompatActivity {
         return true;
     }
 
+
+
     //Sound Test
     public void playSound(View v) {
         mp.start();
+    }
+
+    {
+        if (soundDetector != null) {
+            soundDetector.start();
+        }
     }
 
 }
