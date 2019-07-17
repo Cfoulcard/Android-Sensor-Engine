b09a26e63ccc60df4c4d1c5aab7c5726ae679c7d
Taterchip <cfoulcard@gmail.com>
2019-07-17 17:16:10 -0500

    Temporarily not used
    Will be used to help tidy code more when the project is finish

--- app/src/main/java/com/example/android/soundtechsensors/SoundDetector.java
+++ app/src/main/java/com/example/android/soundtechsensors/SoundDetector.java
@@ -1,19 +1,162 @@
 package com.example.android.soundtechsensors;
 
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.widget.Toast;
-
-import java.io.IOException;
+import android.widget.TextView;
 
 //Audio cannot run background as of Android 9 (API level 28)
 
+public class SoundDetector extends SoundActivity {
+
+    TextView mStatusView;
+    MediaRecorder mRecorder;
+    Thread runner;
+    private static double mEMA = 0.0;
+    static final private double EMA_FILTER = 0.6;
+
+    final Runnable updater = new Runnable(){
+
+        public void run(){
+            updateTv();
+        }
+    };
+    final Handler mHandler = new Handler();
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.sound_sensor);
+
+
+        if (runner == null)
+        {
+            runner = new Thread(){
+                public void run()
+                {
+                    while (runner != null)
+                    {
+                        try
+                        {
+                            Thread.sleep(1000);
+                            Log.i("Noise", "Tock");
+                        } catch (InterruptedException e) { };
+                        mHandler.post(updater);
+                    }
+                }
+            };
+            runner.start();
+            Log.d("Noise", "start runner()");
+        }
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+        startRecorder();
+    }
+
+    public void onPause()
+    {
+        super.onPause();
+        stopRecorder();
+    }
+
+    public void startRecorder(){
+        if (mRecorder == null)
+        {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try
+            {
+                mRecorder.prepare();
+            }catch (java.io.IOException ioe) {
+                android.util.Log.e("[Monkey]", "IOException: " +
+                        android.util.Log.getStackTraceString(ioe));
+
+            }catch (java.lang.SecurityException e) {
+                android.util.Log.e("[Monkey]", "SecurityException: " +
+                        android.util.Log.getStackTraceString(e));
+            }
+            try
+            {
+                mRecorder.start();
+            }catch (java.lang.SecurityException e) {
+                android.util.Log.e("[Monkey]", "SecurityException: " +
+                        android.util.Log.getStackTraceString(e));
+            }
+
+            //mEMA = 0.0;
+        }
+
+    }
+
+    public void stopRecorder() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public void updateTv(){
+        mStatusView.setText(Double.toString((getAmplitudeEMA())) + " dB");
+    }
+
+    public double soundDb(double ampl) {
+        return  20 * Math.log10(getAmplitudeEMA() / ampl);
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  (mRecorder.getMaxAmplitude());
+        else
+            return 0;
+    }
+    public int getAmplitudeEMA() {
+        double amp =  getAmplitude();
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
+        return (int) mEMA;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 public class SoundDetector {
 
     private MediaRecorder mRecorder = null;
+
+    TextView mStatusView;
+    private static double mEMA = 0.0;
+    static final private double EMA_FILTER = 0.6;
+
+
 
 
 
@@ -44,7 +187,13 @@ public class SoundDetector {
         }
     }
 
+    public void updateTv(){
+        mStatusView.setText(Double.toString((getAmplitudeEMA())) + " dB");
+    }
 
+    public double soundDb(double ampl){
+        return  20 * Math.log10(getAmplitudeEMA() / ampl);
+    }
 
     public double getAmplitude() {
         if (mRecorder != null)
@@ -53,4 +202,11 @@ public class SoundDetector {
             return 0;
         }
     }
+
+    public double getAmplitudeEMA() {
+        double amp =  getAmplitude();
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
+        return mEMA;
+    }
 }
+*/
