ed6e792251f4b742f59724b0cece13172931c091
Taterchip <cfoulcard@gmail.com>
2019-10-23 21:03:13 -0500

    Foundation structure

--- app/src/main/java/com/example/android/soundtechsensors/LightActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/LightActivity.java
@@ -2,13 +2,39 @@ package com.example.android.soundtechsensors;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class LightActivity extends AppCompatActivity {
+
+    TextView currentlux;
+
+    // Initiate Firebase Analystics
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lux_sensor);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        //Textviews
+        currentlux = (TextView) findViewById(R.id.current_lux);
+
     }
+
+    //This will add functionality to the menu button within the action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
+    }
+
 }
