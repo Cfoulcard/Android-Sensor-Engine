e649e6d0b3bed4dcff214f1186530572aee33197
Taterchip <cfoulcard@gmail.com>
2019-10-27 15:34:17 -0500

    Create TemperatureActivity.java
    Working, need a couple future tweaks

---
+++ app/src/main/java/com/example/android/soundtechsensors/TemperatureActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.soundtechsensors;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+// TODO inform if phone does not support sensor
+// TODO add menu
+
+public class TemperatureActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView currentDegrees;
+    private SensorManager sensorManager;
+    private Sensor temperature;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phone_temp_sensor);
+
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        temperature = sensorManager.getDefaultSensor((Sensor.TYPE_AMBIENT_TEMPERATURE));
+        currentDegrees = (TextView) findViewById(R.id.current_temp);
+
+
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    //Main magic of the Temperature Sensor
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE)
+        {
+            currentDegrees.setText(event.values[0] + " °C" );
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
+
