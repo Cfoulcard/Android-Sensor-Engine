564d781cc23a0b479d96807b73b3b5ebf715626f
Taterchip <cfoulcard@gmail.com>
2019-11-02 20:37:36 -0500

    Working Ram Sensor
    Working, need to add extra kicks for the future

--- app/src/main/java/com/example/android/soundtechsensors/RamActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/RamActivity.java
@@ -1,10 +1,9 @@
 package com.example.android.soundtechsensors;
 
+import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,36 +11,39 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import static com.example.android.soundtechsensors.R.layout.ram_sensor;
+
 public class RamActivity extends AppCompatActivity {
 
     TextView currentRam;
-    TextView realCurrentRam;
     private SensorManager sensorManager;
     private Sensor ram;
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ram_sensor);
-
-
-        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
-        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        activityManager.getMemoryInfo(mi);
-        long availableMegs = mi.availMem / 0x100000L;
-
-//Percentage can be calculated for API 16+
-        double percentAvail = mi.availMem / (double)mi.totalMem * 100.0;
-
+        setContentView(ram_sensor);
 
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
         currentRam = (TextView) findViewById(R.id.current_ram);
-        // realCurrentRam = (TextView) findViewById(R.id.ram);
+        String i = String.valueOf(getMemorySize());
+        currentRam.setText((i) + " mB");
 
-        currentRam.setText((int) availableMegs);
+    }
 
+    public int getMemorySize() {
+        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        activityManager.getMemoryInfo(mi);
+        double availableMegs = mi.availMem / 0x100000L;
+
+//Percentage can be calculated for API 16+
+        double percentAvail = mi.availMem / (double)mi.totalMem * 100.0;
+
+        return (int) availableMegs;
     }
 
 }
