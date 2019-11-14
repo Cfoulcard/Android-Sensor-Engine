4bcb932061c423bddb3426efa278559728a75f7a
Taterchip <cfoulcard@gmail.com>
2019-11-13 21:19:10 -0600

    Added Firebase and Beautified Code

--- app/src/main/java/com/example/android/soundtechsensors/BatteryActivity.kt
+++ app/src/main/java/com/example/android/soundtechsensors/BatteryActivity.kt
@@ -8,20 +8,25 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.battery_sensor.*
 
 class BatteryActivity : AppCompatActivity() {
 
-    // public Context context;
     internal lateinit var currentBattery: TextView
     private val context: Context? = null
     private val mBatteryLevel: Int = 0
     private var ifilter: IntentFilter? = null
    // private val level = registerMyReceiver()
 
+    // Initiate Firebase Analytics
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
+    //Initiate Broadcast Receiver to utilize BatteryManager Features
+    //Is parsed by registerMyReceiver() to the textview
     private var mBatteryReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
@@ -31,28 +36,38 @@ class BatteryActivity : AppCompatActivity() {
 
             currentBattery.text = "$level"
 
-
-
            // Toast.makeText(context, "Current Battery Level: $level", Toast.LENGTH_LONG).show()
         }
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.battery_sensor)
 
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
         currentBattery = findViewById(R.id.current_battery)
         currentBattery.text = "${registerMyReceiver()} percent"
 
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     private fun registerMyReceiver(): BroadcastReceiver {
         ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         registerReceiver(mBatteryReceiver, ifilter)
 
         return mBatteryReceiver
+    }
+
+    //This will add functionality to the menu button within the action bar
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.navigation_menu, menu)
+        return true
     }
 }
