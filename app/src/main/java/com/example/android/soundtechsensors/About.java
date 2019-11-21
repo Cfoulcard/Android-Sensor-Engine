a647a214cfbbbff391bf85517f7bf20d5912e3cd
Taterchip <cfoulcard@gmail.com>
2019-11-20 22:42:47 -0600

    Create About.java

---
+++ app/src/main/java/com/example/android/soundtechsensors/About.java
@@ -0,0 +1,14 @@
+package com.example.android.soundtechsensors;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+    }
+}
