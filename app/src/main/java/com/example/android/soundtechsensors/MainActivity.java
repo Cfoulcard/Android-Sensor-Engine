4c22cc5babe7ff7e97564be117eddfe63e6f6425
Taterchip <cfoulcard@gmail.com>
2019-11-23 21:50:50 -0600

    Added UI animations

--- app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.soundtechsensors;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -19,8 +20,12 @@ import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
-//TODO: Make going into other Activities responsive
+
     //TODO: Turn off sensors when not in use
+    //TODO: Transitions/Animations/UI Work
+
+    //FUTURE IDEAS
+    //Add new activity explaining what sensors are available on user's device
 
     // Initiate Firebase Analytics
     private FirebaseAnalytics mFirebaseAnalytics;
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.sensor_selection);
 
         // This will make the Status Bar completely transparent
@@ -80,37 +86,75 @@ public class MainActivity extends AppCompatActivity {
 
     public void soundIconIntent(View view) {
         Intent soundIntent = new Intent(this, SoundSensorActivity.class);
-        this.startActivity(soundIntent);
+
+        ImageView sharedView = findViewById(R.id.sound_icon);
+        String transitionName = getString(R.string.sound_anim);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
+        this.startActivity(soundIntent, transitionActivityOptions.toBundle());
+        
+       // this.startActivity(soundIntent);
         // Toast.makeText(view.getContext(),"Button Clicked",Toast.LENGTH_LONG).show();
     }
 
     public void tempIconIntent(View view) {
         Intent tempIntent = new Intent(this, TemperatureActivity.class);
-        this.startActivity(tempIntent);
+
+        ImageView sharedView = findViewById(R.id.temp_icon);
+        String transitionName = getString(R.string.temp_anim);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
+        this.startActivity(tempIntent, transitionActivityOptions.toBundle());
+
+       // this.startActivity(tempIntent);
         // Toast.makeText(view.getContext(),"Button Clicked",Toast.LENGTH_LONG).show();
     }
 
     public void lightIconIntent(View view) {
         Intent lightIntent = new Intent(this, LightSensorActivity.class);
-        this.startActivity(lightIntent);
+
+        ImageView sharedView = findViewById(R.id.light_icon);
+        String transitionName = getString(R.string.light_anim);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
+        this.startActivity(lightIntent, transitionActivityOptions.toBundle());
+       // this.startActivity(lightIntent);
         // Toast.makeText(view.getContext(),"Button Clicked",Toast.LENGTH_LONG).show();
     }
 
     public void ramIconIntent(View view) {
         Intent ramIntent = new Intent(this, RamActivity.class);
-        this.startActivity(ramIntent);
+
+        ImageView sharedView = findViewById(R.id.ram_icon);
+        String transitionName = getString(R.string.ram_anim);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
+        this.startActivity(ramIntent, transitionActivityOptions.toBundle());
+    //    this.startActivity(ramIntent);
         // Toast.makeText(view.getContext(),"Button Clicked",Toast.LENGTH_LONG).show();
     }
 
     public void batteryIconIntent(View view) {
-        Intent soundIntent = new Intent(this, BatteryActivity.class);
-        this.startActivity(soundIntent);
+        Intent batteryIntent = new Intent(this, BatteryActivity.class);
+
+        ImageView sharedView = findViewById(R.id.battery_icon);
+        String transitionName = getString(R.string.battery_anim);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
+        this.startActivity(batteryIntent, transitionActivityOptions.toBundle());
+     //   this.startActivity(soundIntent);
         // Toast.makeText(view.getContext(),"Button Clicked",Toast.LENGTH_LONG).show();
     }
 
     public void speedIconIntent(View view) {
         Intent speedIntent = new Intent(this, AccelerometerActivity.class);
-        this.startActivity(speedIntent);
+
+        ImageView sharedView = findViewById(R.id.speed_icon);
+        String transitionName = getString(R.string.speed_anim);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
+        this.startActivity(speedIntent, transitionActivityOptions.toBundle());
+     //   this.startActivity(speedIntent);
         // Toast.makeText(view.getContext(),"Button Clicked",Toast.LENGTH_LONG).show();
     }
 }
