59fb8ba2e0cbef291fe76c758d87b94e30d07705
Taterchip <cfoulcard@gmail.com>
2019-11-23 23:48:13 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
+++ app/src/main/java/com/example/android/soundtechsensors/MainActivity.java
@@ -21,8 +21,16 @@ import java.util.prefs.Preferences;
 public class MainActivity extends AppCompatActivity {
 
 
-    //TODO: Turn off sensors when not in use
-    //TODO: Transitions/Animations/UI Work
+    //TODO: Turn off sensors when not in use (Battery, Speed, Ram)
+    //TODO: Add animated launch screen?
+    //TODO: Add F for Temp Sensor (Kelvin?)
+    //TODO: Add km/h for speed
+    //TODO: Create about and credits menu activities
+    //TODO: Make Preferences for the above conversions
+    //TODO: Show user a list of sensors their device can use
+    //TODO: Tidy code and reduce build size
+    //TODO: Plan Google Play launch
+
 
     //FUTURE IDEAS
     //Add new activity explaining what sensors are available on user's device
