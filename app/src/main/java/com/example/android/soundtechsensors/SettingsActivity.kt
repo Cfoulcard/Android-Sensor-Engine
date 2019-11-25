226e3bc787a28d2dd9b322ec717321d0fe40f93d
Taterchip <cfoulcard@gmail.com>
2019-11-24 23:17:01 -0600

    update

---
+++ app/src/main/java/com/example/android/soundtechsensors/SettingsActivity.kt
@@ -0,0 +1,29 @@
+package com.example.android.soundtechsensors
+
+import android.os.Bundle
+import android.preference.PreferenceFragment
+import android.support.v7.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (fragmentManager.findFragmentById(android.R.id.content) == null) {
+            fragmentManager.beginTransaction()
+                    .add(android.R.id.content, SettingsFragment()).commit()
+        }
+    }
+
+
+    class SettingsFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.preferences)
+
+            (findPreference("ringtone"))
+            (findPreference("text"))
+
+        }
+    }
+}
