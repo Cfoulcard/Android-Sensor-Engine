c7559d4a309c956f708840822dc031af077b2a02
Taterchip <cfoulcard@gmail.com>
2019-12-16 19:35:27 -0600

    Design and Code for Pressure Sensor

---
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/Sensors/PressureActivity.java
@@ -0,0 +1,105 @@
+package com.christianfoulcard.android.androidsensorengine.Sensors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.christianfoulcard.android.androidsensorengine.R;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class PressureActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView pressure_text;
+    TextView currentPressure;
+    TextView pressureLevel;
+    private SensorManager sensorManager;
+    private Sensor pressure;
+    private Context mContext;
+    private Activity mActivity;
+    private SharedPreferences mSharedPreferences;
+
+    // Initiate Firebase Analytics
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pressure_sensor);
+
+        pressure_text = (TextView) findViewById(R.id.pressure);
+        currentPressure = (TextView) findViewById(R.id.current_pressure);
+        pressureLevel = (TextView) findViewById(R.id.pressure_sensor);
+
+        final Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(1500);
+        pressure_text.startAnimation(in);
+        currentPressure.startAnimation(in);
+        pressureLevel.startAnimation(in);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // Get an instance of the sensor service, and use that to get an instance of
+        // the relative temperature. If device does not support this sensor a toast message will
+        // appear
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) == null) {
+            Toast.makeText(this, "Your device does not support this sensor", Toast.LENGTH_LONG).show();
+        }
+
+        // Ambient Temperature measures the temperature around the device
+        pressure = sensorManager.getDefaultSensor((Sensor.TYPE_PRESSURE));
+        currentPressure = (TextView) findViewById(R.id.current_pressure);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // Get the application context
+        mContext = getApplicationContext();
+
+        // Get the activity
+        mActivity = PressureActivity.this;
+
+        int pressure_level = (int) event.values[0];
+
+        //The default Android properties for event.values[0] is the formula for Celsius
+        //This is for Fahrenheit
+        if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+            currentPressure.setText(pressure_level + " hPa");
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
+
