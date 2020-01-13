32044fc378d3e3a95efa03f6e229be42107aa95c
Taterchip <cfoulcard@gmail.com>
2020-01-12 19:50:53 -0600

    Fixed back button issues

--- app/src/main/java/com/christianfoulcard/android/androidsensorengine/Preferences/SettingsActivity.java
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/Preferences/SettingsActivity.java
@@ -29,6 +29,12 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
