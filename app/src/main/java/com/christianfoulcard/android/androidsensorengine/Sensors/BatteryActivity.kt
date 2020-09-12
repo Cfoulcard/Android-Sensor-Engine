7c0cebcfc186a4b2de3264b4dc923c29abc6049b
Taterchip <cfoulcard@gmail.com>
2020-09-11 19:37:23 -0500

    Update BatteryActivity.kt

--- app/src/main/java/com/christianfoulcard/android/androidsensorengine/Sensors/BatteryActivity.kt
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/Sensors/BatteryActivity.kt
@@ -86,8 +86,10 @@ class BatteryActivity : AppCompatActivity() {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         //Opens Pin Shortcut menu after long pressing the logo
-        batteryLogo!!.setOnLongClickListener() {
-            sensorShortcut()
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            batteryLogo!!.setOnLongClickListener() {
+                sensorShortcut()
+            }
         }
 
        // currentBattery = findViewById(R.id.current_battery)
@@ -299,27 +301,19 @@ class BatteryActivity : AppCompatActivity() {
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Adds Pin Shortcut Functionality
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun sensorShortcut(): Boolean {
-        val shortcutManager = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
-            getSystemService<ShortcutManager>(ShortcutManager::class.java)
-        } else {
-            TODO("VERSION.SDK_INT < N_MR1")
-        }
+    fun sensorShortcut(): Boolean {
 
-        val batteryIntent = Intent(this, BatteryActivity::class.java)
+        val shortcutManager = getSystemService<ShortcutManager>(ShortcutManager::class.java)
+        val intent = Intent(this, BatteryActivity::class.java)
                 .setAction("Battery")
 
-        if (if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    shortcutManager!!.isRequestPinShortcutSupported
-                } else {
-                    TODO("VERSION.SDK_INT < O")
-                }) {
+        if (shortcutManager!!.isRequestPinShortcutSupported) {
 
             val pinShortcutInfo = ShortcutInfo.Builder(this, "battery-shortcut")
                     .setShortLabel(getString(R.string.battery_sensor))
                     .setLongLabel(getString(R.string.battery_sensor))
                     .setIcon(Icon.createWithResource(this, R.drawable.battery_icon))
-                    .setIntent(batteryIntent)
+                    .setIntent(intent)
                     .build()
 
             // Create the PendingIntent object only if your app needs to be notified
