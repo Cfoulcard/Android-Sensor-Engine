e546823de314f74f2141e5153c3f825cc4f8d859
Taterchip <cfoulcard@gmail.com>
2020-09-11 19:37:33 -0500

    Update WalkActivity.kt

--- app/src/main/java/com/christianfoulcard/android/androidsensorengine/Sensors/WalkActivity.kt
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/Sensors/WalkActivity.kt
@@ -90,8 +90,10 @@ class WalkActivity : AppCompatActivity(), SensorEventListener {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         //Opens Pin Shortcut menu after long pressing the logo
-        walkLogo!!.setOnLongClickListener() {
-            sensorShortcut()
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            walkLogo!!.setOnLongClickListener() {
+                sensorShortcut()
+            }
         }
 
         // Get an instance of the sensor service, and use that to get an instance of
@@ -180,27 +182,19 @@ class WalkActivity : AppCompatActivity(), SensorEventListener {
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Adds Pin Shortcut Functionality
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun sensorShortcut(): Boolean {
-        val shortcutManager = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
-            getSystemService<ShortcutManager>(ShortcutManager::class.java)
-        } else {
-            TODO("VERSION.SDK_INT < N_MR1")
-        }
+    fun sensorShortcut(): Boolean {
 
-        val walkIntent = Intent(this, WalkActivity::class.java)
+        val shortcutManager = getSystemService<ShortcutManager>(ShortcutManager::class.java)
+        val intent = Intent(this, WalkActivity::class.java)
                 .setAction("Walk")
 
-        if (if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    shortcutManager!!.isRequestPinShortcutSupported
-                } else {
-                    TODO("VERSION.SDK_INT < O")
-                }) {
+        if (shortcutManager!!.isRequestPinShortcutSupported) {
 
             val pinShortcutInfo = ShortcutInfo.Builder(this, "walk-shortcut")
                     .setShortLabel(getString(R.string.walk_sensor))
                     .setLongLabel(getString(R.string.walk_sensor))
                     .setIcon(Icon.createWithResource(this, R.drawable.walk_icon))
-                    .setIntent(walkIntent)
+                    .setIntent(intent)
                     .build()
 
             // Create the PendingIntent object only if your app needs to be notified
