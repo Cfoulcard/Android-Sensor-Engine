2fb750451cbe225c65992a54d74e78711d732cea
Taterchip <cfoulcard@gmail.com>
2020-10-25 21:38:00 -0500

    testing

--- app/src/test/java/com/christianfoulcard/android/androidsensorengine/TestMainActivity.kt
+++ app/src/test/java/com/christianfoulcard/android/androidsensorengine/TestMainActivity.kt
@@ -1,9 +1,30 @@
 package com.christianfoulcard.android.androidsensorengine
 
-import android.net.sip.SipSession
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.app.ActivityOptions
+import android.content.Intent
+import android.provider.Settings.Global.getString
+import android.view.View
+import android.widget.ImageView
+import com.christianfoulcard.android.androidsensorengine.Sensors.SoundSensorActivity
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+// @SmallTest
+        public class TestMainActivity {
+
+           private var mainActivity: MainActivity? = null
+
+    @Before
+    open fun setUp() { mainActivity = MainActivity() }
+
+    @Test
+    open fun intentTracker() {
+        mainActivity?.humidityIconIntent(view = null)
+    }
 
 
 
+        }
