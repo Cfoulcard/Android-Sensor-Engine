1a77660d85d49ed190fa720866a804ebe1a48b40
Christian Foulcard <cfoulcard@gmail.com>
2021-02-03 17:16:54 -0600

    Update Constants.kt

--- app/src/main/java/com/christianfoulcard/android/androidsensorengine/Constants.kt
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/Constants.kt
@@ -5,10 +5,3 @@ const val CHANNEL_ID = "123"
 const val VERBOSE_NOTIFICATION_CHANNEL_NAME = "Work Notification"
 const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION = "Work Notification Testing"
 
-//Used to help with Sound Sensor's Audio parsing
-var EMA = 0.0
-const val EMA_FILTER = 0.6
-
-//Used for record audio permission
-const val AUDIO_RECORD_REQUEST_CODE = 122
-const val MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 99
