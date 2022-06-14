b9f0335c3e6d203be090ccbece9d09c779203a77
Christian Foulcard <cfoulcard@gmail.com>
2022-06-13 20:57:34 -0500

    Added Log Utils for debugging

---
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/utils/LogUtils.kt
@@ -0,0 +1,24 @@
+package com.christianfoulcard.android.androidsensorengine.utils
+
+/** Simple Tag Object to return the class name throughout the app */
+object LogUtils {
+
+    val Any.TAG: String
+        get() {
+            return when (true) {
+                !javaClass.isAnonymousClass -> {
+                    val name = javaClass.simpleName
+                    if (name.length <= 23) name else name.substring(0, 23)// first 23 chars
+                }
+                javaClass.isAnonymousClass -> {
+                    val name = javaClass.enclosingClass.simpleName
+                    if (name.length <= 23) name else name.substring(name.length - 23, name.length)// last 23 chars
+                }
+                else -> {
+                    val name = javaClass.name
+                    if (name.length <= 23) name else name.substring(name.length - 23, name.length)// last 23 chars
+                }
+            }
+        }
+
+}
