b6ee423cb0b4478af0c7216b97d055ac15e6a1c2
Christian Foulcard <cfoulcard@gmail.com>
2022-06-26 12:07:18 -0500

    Deleted classes

--- app/src/main/java/com/christianfoulcard/android/androidsensorengine/Constants.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("Constants")
-
-//Notification ID
-const val CHANNEL_ID = "123"
-const val VERBOSE_NOTIFICATION_CHANNEL_NAME = "Work Notification"
-const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION = "Work Notification Testing"
-
